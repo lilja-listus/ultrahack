d0931b6d050a828dd1cfcaf4f3ba2cc854c95df7
Kelvin Jackson <kechpaja@comcast.net>
2018-05-19 00:48:00 +0300

    Connection to DB basically working; some functions implemented

--- src/backend/calldb/calldb.py
+++ src/backend/calldb/calldb.py
@@ -3,26 +3,48 @@
 # databases, even if they are radically different (i.e. SQL versus NoSQL).
 #####
 
+import pyodbc
 
 
+###
+# Magic Values
+###
+
+dburl = "vastomadb.c9xefv9wj6wi.eu-central-1.rds.amazonaws.com"
+dbpw = "vastomapassword" # TODO get this out of the repo
+dbname = "vastomadb"
+
+# TODO DSN?
 
 class DatabaseWrapper(object):
     def __init__(self):
-        pass # TODO create DB as self.db
+        # TODO this is ugly, but we only really need it here for testing
+        self.connection = pyodbc.connect("DRIVER={MySQL ODBC 8.0 Driver};SERVER=vastomadb.c9xefv9wj6wi.eu-central-1.rds.amazonaws.com;UID=vastomadbuser;PWD=vastomapassword")
+
+        # XXX pyodbc documentation implies that this will work
+        self.connection.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
+        self.connection.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
+        self.connection.setencoding(encoding='utf-8')
+
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("use " + dbname + ";")
+
+    # Just a shorter name for convenience
+    def q(self, *query, maxrows=1):
+        self.cursor.execute(*query)
+        return self.cursor.fetchmany(maxrows)
+
+    def qUserWhere(self, col, val):
+        rows = self.q("select id, name, home from users where "+col+" = ?", val)
+        if len(rows) == 0:
+            return None
+        return {"id" : rows[0].id, "name" : rows[0].name, "home" : rows[0].home}
 
     def getUserInfo(self, user):
-        # TODO actually query some DB!
-        return { 
-                   "id" : user,
-                   "name" : "A girl has no name",
-                   "home" : {
-                       "id" : "westeros_winterfell",
-                       "name" : "Winterfell"
-                   }
-               }
+        return self.qUserWhere("id", user)
 
     def getUserByEmail(self, email):
-        return "1234" # TODO get actual user ID, or None if no such user
+        return self.qUserWhere("email", email)
 
     # XXX this returns a list of user objects, not just IDs
     # Returns None on error
