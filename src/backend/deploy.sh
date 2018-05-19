e376416c8756132ac06ee0c4a803f15d00a21b83
Kelvin Jackson <kechpaja@comcast.net>
2018-05-19 11:33:34 +0300

    Deploy script, first version

---
+++ src/backend/deploy.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+copy="scp -i ~/real_vastoma_aws_keystxt.pem"
+
+dest="ec2-user@ec2-34-230-45-89.compute-1.amazonaws.com:/var/www/api/"
+
+src="." # TODO get this from script location?
+
+$copy "$src/endpoints.py" "$dest"
+$copy "$src/calldb/calldb.py" "$dest/calldb"
+$copy "$src/calldb/__init__.py" "$dest/calldb"
