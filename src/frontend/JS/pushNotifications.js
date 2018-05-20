cd6123ad29f102738079d93557a25ca7c5df7d5a
Kelvin Jackson <kechpaja@comcast.net>
2018-05-20 03:56:59 +0300

    Some front-end code for push notifs

---
+++ src/frontend/JS/pushNotifications.js
@@ -0,0 +1,65 @@
+(function () {
+
+    // Code closely follows that of 
+    // https://developers.google.com/web/fundamentals/push-notifications
+
+    if (!("serviceWorker" in navigator) || !("PushManager" in window)) {
+        return; // TODO can't use push notification in this browser
+    }
+
+    function registerServiceWorker() {
+        return navigator.serviceWorker.register("JS/serviceWorker.js")
+            .then(function(registration) {
+                return registration; // Success!
+            }).catch(function(err) {
+                console.error("Service worker registration failed.", err);
+            });
+    }
+
+    function getUserPermission() {
+        return new Promise(function(resolve, reject) {
+            const permission = Notification.requestPermission(function(result) {
+                resolve(result);
+            });
+
+            if (permission) {
+                permission.then(resolve, reject);
+            }
+        }).then(function(permission) {
+            if (permission !== "granted") {
+                throw new Error("Permission not granted.");
+            }
+        });
+    }
+
+    function subscribeToPush() {
+        return navigator.serviceWorker.register("JS/serviceWorker.js")
+            .then(function(registration) {
+                return registration.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: urlBase64ToUint8Array(
+                        "BNIsL4znqW3WbwMBSkZyQQ5Hb3urTmatxOBOgmtEZ03ux6I9LYzCh"
+                        + "h6X2-_NSI7HZLeCb8JnHBqTCknQn-i0kws"
+                    )
+                });
+            ).then(function(pushSubscription) {
+                return pushSubscription;
+            });
+    }
+
+    function callSavePushEndpoint(subscription) {
+        return fetch("api/save_push_subscription", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(subscription)
+        }).then(function(response) {
+            if (!response.ok) { throw new Error("callSavePushEndpoint 1"); }
+            return response.json();
+        }).then(function(responseData) {
+            // XXX Nothing in particular to do here
+        });
+    }
+
+})();
---
+++ src/frontend/JS/serviceWorker.js
@@ -0,0 +1,19 @@
+self.addEventListener("push", function(event) {
+    if (event.data) {
+        var data = event.data.json();
+        
+        var title = "Vastoma Overlap Notification"; // TODO localize
+        var body;
+        if (data.type === "overlap") {
+            // TODO localize
+            body = data.data.users.join(" and ") + " will overlap in "
+                 + data.data.destination + " from " + data.data.start
+                 + " to " + data.data.end "."; // TODO dates human-readable
+        }
+        event.waitUntil(self.registration.showNotification(title, {
+            body: body
+        });
+    } else {
+        // TODO fail in some useful way?
+    }
+});
